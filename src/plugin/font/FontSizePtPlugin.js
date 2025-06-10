e6cf822237ffb351ae1d34b74dec60a27a396e71
bloodthirstyleo <bloodthirstyleo@gmail.com>
2025-06-10 07:20:54 +0700

    chore: bump version to 0.1.4 in package.json
    fix: remove downcast conversion for fontSize in FontSizePtPlugin

--- src/plugin/font/FontSizePtPlugin.js
+++ src/plugin/font/FontSizePtPlugin.js
@@ -5,8 +5,6 @@ export default class FontSizePtPlugin extends Plugin {
     const editor = this.editor;
     const conversion = editor.conversion;
 
-    conversion.for("downcast").elementToAttribute.delete("fontSize");
-
     conversion.for("downcast").attributeToElement({
       model: "fontSize",
       view: (modelAttributeValue) => {
